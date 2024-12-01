cdaf6265d133d336f442e2f7cc6fd2e5c0afc23f
felixyuu <128110495+FelixU-github@users.noreply.github.com>
2024-12-01 18:23:01 +0800

    Fix Solution12

--- Solution12.java
+++ Solution12.java
@@ -4,7 +4,7 @@ import java.util.*;
  * ## 字符串相乘
  * 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
  * 注意：不能使用任何内置的 BigInteger 库或直接将输入转换为整数。
- * 
+ *
  *  示例 1:
  *  输入: num1 = "2", num2 = "3"
  * 输出: "6"
@@ -17,14 +17,17 @@ import java.util.*;
 
 class Solution {
     public String multiply(String num1, String num2) {
-        if (num1.equals("0") | num2.equals("0")) {
+        if (num1.equals("0") || num2.equals("0")) {
             return "0";
         }
-        String ans = "0"
+
+        String ans = "0"; //修改： 缺少分号
         int m = num1.length(), n = num2.length();
+
         for (int i = n - 1; i >= 0; i--) {
-            StringBuffer curr = new StringBuffer();
+            StringBuilder curr = new StringBuilder();
             int add = 0;
+            // 补充后缀零
             for (int j = n - 1; j > i; j--) {
                 curr.append(0);
             }
@@ -35,17 +38,18 @@ class Solution {
                 curr.append(product % 10);
                 add = product / 10;
             }
+
             if (add != 0) {
-                curr.append(add % 10);
+                curr.append(add);
             }
-            ans == addStrings(ans, curr.reverse().toString());
+            ans = addStrings(ans, curr.reverse().toString());// 修正：改为赋值操作，而不是比较操作
         }
         return ans;
     }
 
     public String addStrings(String num1, String num2) {
         int i = num1.length() - 1, j = num2.length() - 1, add = 0;
-        StringBuffer ans = new StringBuffer();
+        StringBuilder ans = new StringBuilder();
         while (i >= 0 || j >= 0 || add != 0) {
             int x = i >= 0 ? num1.charAt(i) - '0' : 0;
             int y = j >= 0 ? num2.charAt(j) - '0' : 0;
@@ -55,7 +59,6 @@ class Solution {
             i--;
             j--;
         }
-        ans.reverse();
-        return ans.toString();
+        return ans.reverse().toString();
     }
 }
